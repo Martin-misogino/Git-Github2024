12563242992422edac4e94968b4879d52a36334f
Martín Silveira de Almeida <martin.2022311000@aluno.iffar.edu.br>
2024-03-18 16:24:42 -0300

    criei o formulario de cadastro

---
+++ formcadastrar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="cadastrar.php" method="post">
+        Email: <input type="text" name="email"><br>
+        Senha: <input type="text" name="senha"><br>
+        <input type="submit" value="Cadastrar"><br>
+    </form>
+</body>
+</html>
